e23b5e64e339ef75f4741ab71a884dcb9e9cd90e
takatoshi.ichige <tichige1213@gmail.com>
2021-07-30 16:03:08 +0900

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by S-wayMock2 on 2021/07/30.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
